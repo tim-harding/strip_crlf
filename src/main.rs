8be0683b380cc6a40ca3c2e98612822087849a5a
Tim Harding <Tim@TimHarding.co>
2021-09-29 20:56:45 -0700

    Unescaped and escaped versions

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,57 @@
+use clap::Clap;
 use std::io::{self, Read};
 
+#[derive(Clap, Copy, Clone, PartialEq, Eq)]
+struct Options {
+    #[clap(short, long)]
+    escaped: bool,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let options: Options = Options::parse();
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
-    let mut iter = buffer.chars().peekable();
+    if options.escaped {
+        escaped(&buffer);
+    } else {
+        unescaped(&buffer);
+    }
+    Ok(())
+}
+
+fn unescaped(stdin: &str) {
+    let mut iter = stdin.chars().peekable();
     let mut out = String::new();
     while let Some(next) = iter.next() {
         match next {
             '\r' => {
-                if iter.peek() == Some(&'\n') {
-                    let _ = iter.next();
+                // Do nothing.
+            }
+            c => out.push(c),
+        }
+    }
+    println!("{}", out);
+}
+
+fn escaped(stdin: &str) {
+    let mut iter = stdin.chars().peekable();
+    let mut out = String::new();
+    while let Some(next) = iter.next() {
+        match next {
+            '\\' => {
+                if iter.peek() == Some(&'r')
+                    && iter.peek() == Some(&'\\')
+                    && iter.peek() == Some(&'n')
+                {
+                    for _ in 0..3 {
+                        let _ = iter.next();
+                    }
+                    out.push('\\');
+                    out.push('n');
                 }
             }
             c => out.push(c),
         }
     }
     println!("{}", out);
-    Ok(())
 }
