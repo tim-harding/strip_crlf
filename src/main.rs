e2a8538f3db23a1e7bc1af65778e8ba4c6114d7b
Tim Harding <Tim@TimHarding.co>
2021-09-29 20:41:05 -0700

    Wrote program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    let mut iter = buffer.chars().peekable();
+    let mut out = String::new();
+    while let Some(next) = iter.next() {
+        match next {
+            '\r' => {
+                if iter.peek() == Some(&'\n') {
+                    let _ = iter.next();
+                }
+            }
+            c => out.push(c),
+        }
+    }
+    println!("{}", out);
+    Ok(())
 }
